db6cf6cf1d4f7f8a7685b0c4c1b02646e6b08470
Maxim Konovalov <maxim.konovalov@gmail.com>
2025-01-10 19:46:28 -0800

    lcd00: boilerplate

---
+++ lcd00/lcd00.ino
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2025 Maxim Konovalov, maxim.konovalov@gmail.com
+ *
+ * This work is under 2-clause BSD license
+ *
+ * The project was created as part of the game with my son for
+ * the Arduino Uno R3 board.
+ */
+
+/*
+ *
+ */
+
+
+/*
+ * Bootstrap
+ */
+void setup() {
+
+  Serial.begin(9600);
+  Serial.print("*");
+}
+
+void loop() {
+  Serial.print(".");
+}
+
+/* EOF */
